2c37184f52e9df18ae09744f8774a1cee521fb66
Tristan Guest <tristanguest@gmail.com>
2022-06-06 22:56:22 -0300

    Update copy

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>{new Date().getFullYear()} &copy; Synergy Coaching.</p>
+      <p>{new Date().getFullYear()} &copy; Synergy Coaching and Wellness.</p>
     </footer>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -29,9 +29,7 @@ export default function Home() {
         </div>
       </div>
       <div className={styles.contact_refer_content}>
-        <h3>
-          Reserve a spot, or ask a question. I&apos;d love to hear from you.
-        </h3>
+        <h3>Book coaching, a retreat or ask a question</h3>
         <Link href={"/contact"}>
           <button className={styles.contact_button}>
             <a className={styles.nav_link}>Get in touch</a>
