6e259d4791b86bdfebb867d98047b82903fb5ae5
Tristan Guest <tristanguest@gmail.com>
2022-06-06 22:18:07 -0300

    Sendgrid api integration

--- components/ContactForm/ContactForm.jsx
+++ components/ContactForm/ContactForm.jsx
@@ -1,34 +1,152 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ContactForm.module.css";
 
 const ContactForm = ({ children }) => {
+  // States for contact form fields
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  //   Form validation state
+  const [errors, setErrors] = useState({});
+
+  //   Setting button text on form submission
+  const [buttonText, setButtonText] = useState("Send");
+
+  // Setting success or failure messages states
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showFailureMessage, setShowFailureMessage] = useState(false);
+
+  // Validation check method
+  const handleValidation = () => {
+    let tempErrors = {};
+    let isValid = true;
+
+    if (fullname.length <= 0) {
+      tempErrors["fullname"] = true;
+      isValid = false;
+    }
+    if (email.length <= 0) {
+      tempErrors["email"] = true;
+      isValid = false;
+    }
+    if (subject.length <= 0) {
+      tempErrors["subject"] = true;
+      isValid = false;
+    }
+    if (message.length <= 0) {
+      tempErrors["message"] = true;
+      isValid = false;
+    }
+
+    setErrors({ ...tempErrors });
+    console.log("errors", errors);
+    return isValid;
+  };
+
+  //   Handling form submit
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let isValidForm = handleValidation();
+
+    if (isValidForm) {
+      setButtonText("Sending");
+      const res = await fetch("/api/sendgrid", {
+        body: JSON.stringify({
+          email: email,
+          fullname: fullname,
+          subject: subject,
+          message: message,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      });
+
+      console.log("res", res);
+
+      const { error } = await res.json();
+      if (error) {
+        console.log(error);
+        setShowSuccessMessage(false);
+        setShowFailureMessage(true);
+        setButtonText("Send");
+        return;
+      }
+      setShowSuccessMessage(true);
+      setShowFailureMessage(false);
+      setButtonText("Send");
+    }
+    console.log(fullname, email, subject, message);
+  };
+
   return (
     <>
-      <form className={styles.form}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <h1>Send a message</h1>
 
         <label htmlFor="fullname" className={styles.form_label}>
           Full name<span className={styles.highlight}>*</span>
         </label>
-        <input type="text" name="fullname" className={styles.input} />
+        <input
+          type="text"
+          name="fullname"
+          className={styles.input}
+          value={fullname}
+          onChange={(e) => {
+            setFullname(e.target.value);
+          }}
+        />
 
         <label htmlFor="email" className={styles.form_label}>
           E-mail<span className={styles.highlight}>*</span>
         </label>
-        <input type="email" name="email" className={styles.input} />
+        <input
+          type="email"
+          name="email"
+          className={styles.input}
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
 
         <label htmlFor="subject" className={styles.form_label}>
           Subject<span className={styles.highlight}>*</span>
         </label>
-        <input type="text" name="subject" className={styles.input} />
+        <input
+          type="text"
+          name="subject"
+          className={styles.input}
+          value={subject}
+          onChange={(e) => {
+            setSubject(e.target.value);
+          }}
+        />
 
         <label htmlFor="message" className={styles.form_label}>
           Message<span className={styles.highlight}>*</span>
         </label>
-        <textarea name="message" className={styles.input}></textarea>
+        <textarea
+          name="message"
+          className={styles.input}
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        ></textarea>
         <div className={styles.button_container}>
-          <button className={styles.button}>Send</button>
+          <button className={styles.button}>{buttonText}</button>
         </div>
+        {showSuccessMessage ? (
+          <p>Email sent!</p>
+        ) : showFailureMessage ? (
+          <p>Sending failed...</p>
+        ) : null}
       </form>
     </>
   );
---
+++ pages/api/sendgrid.js
@@ -0,0 +1,51 @@
+import sendgrid from "@sendgrid/mail";
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+async function sendEmail(req, res) {
+  try {
+    // console.log("REQ.BODY", req.body);
+    await sendgrid.send({
+      to: process.env.TO_EMAIL_ADDRESS, // Your email where you'll receive emails
+      from: process.env.FROM_EMAIL_ADDRESS, // your website email address here
+      replyTo: `${req.body.email}`,
+      subject: `SYNERGY COACHING: ${req.body.subject}`,
+      html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html lang="en">
+      <head>
+        <meta charset="utf-8">
+      
+        <title>The HTML5 Herald</title>
+        <meta name="description" content="The HTML5 Herald">
+        <meta name="author" content="SitePoint">
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+      
+        <link rel="stylesheet" href="css/styles.css?v=1.0">
+      
+      </head>
+      
+      <body>
+        <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">              
+              </div>
+              <div class="container" style="margin-left: 20px;margin-right: 20px;">
+              <h3>New mail sent via Synergy Coaching</h3>
+              <div style="font-size: 16px;">
+              <p>From: ${req.body.fullname}</p>
+              <p>Email: ${req.body.email}</p>
+              <p>Message:</p>
+              <p>${req.body.message}</p>
+              <br>
+              </div>
+              </div>
+      </body>
+      </html>`,
+    });
+  } catch (error) {
+    // console.log(error);
+    return res.status(error.statusCode || 500).json({ error: error.message });
+  }
+
+  return res.status(200).json({ error: "" });
+}
+
+export default sendEmail;
