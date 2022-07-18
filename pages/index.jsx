d19ff9de511171e198f7a1512c5a058e790d274c
Tristan Guest <tristanguest@gmail.com>
2022-07-18 20:13:13 -0300

    Pull content from CMS for all pages but contact, retreats

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,42 +1,45 @@
 import { NavButton } from "../components/NavButton";
 import { Splash } from "../components/Splash";
 import styles from "../styles/Home.module.css";
+import { PrismicText, PrismicRichText } from "@prismicio/react";
+import { createClient, imageLoader } from "../prismicio";
 
 export async function getStaticProps() {
+  // Client used to fetch CMS content.
+  const client = createClient();
+
+  // Page document for our homepage from the CMS.
+  const page = await client.getByUID("page", "home");
+
+  // Pass the homepage as prop to our page.
   return {
     props: {
       title: "",
       description:
         "Wild Ocean Coaching and Retreats: Professional coaching services and wellness retreats",
+      page,
     },
   };
 }
 
-export default function Home() {
+export default function Home({ page }) {
   return (
     <div className={styles.container}>
       <Splash />
       <div className={styles.content_container}>
         <div className={styles.welcome_content}>
-          <h1>Life is a journey.</h1>
+          <h1>
+            <PrismicText field={page.data.title} />
+          </h1>
           <hr className={styles.hr} />
-          <p>
-            You can travel this journey with energy, passion and confidence that
-            you are on the right path.
-          </p>
-
-          <p>
-            {
-              "Wild Ocean Coaching and Retreats provides services to help the traveller who needs time and space to revitalize, align their compass, discover inner resources and create momentum to enjoy life's journey."
-            }
-          </p>
-
-          <p>This is an invitation to explore.</p>
+          <PrismicRichText field={page.data.contentMain} />
         </div>
       </div>
       <div className={styles.contact_refer_content}>
         <h2>Book coaching, a retreat or ask a question</h2>
-        <NavButton page="contact">Get in touch</NavButton>
+        <NavButton page="contact">
+          <PrismicText field={page.data.buttonText} />
+        </NavButton>
       </div>
     </div>
   );
