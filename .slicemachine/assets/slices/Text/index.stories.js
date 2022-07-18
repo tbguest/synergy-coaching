161b66fde712057f262636d8baac7033d521735f
Tristan Guest <tristanguest@gmail.com>
2022-07-18 14:14:45 -0300

    Add Prismic silces

---
+++ .slicemachine/assets/slices/Text/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Text';
+
+export default {
+  title: 'slices/Text'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"text","items":[],"primary":{"text":[{"type":"paragraph","text":"Qui velit nisi officia consectetur voluptate quis incididunt aliqua. Sit ad sunt sint aute. Non quis sit adipisicing sunt non.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
